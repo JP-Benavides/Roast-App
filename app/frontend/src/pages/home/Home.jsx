5ba1e8639f0409253ff2e1807274f15c4c7c7d2d
JP-Benavides <149683039+JP-Benavides@users.noreply.github.com>
2024-12-05 00:45:02 -0500

    Added MapBox Api

--- app/frontend/src/pages/home/Home.jsx
+++ app/frontend/src/pages/home/Home.jsx
@@ -1,10 +1,15 @@
 import React from 'react'; 
 import './Home.css';
 import Navbar from '../sharedComponents/Navbar.jsx';
+import Map from './HomeComponents/Map.jsx';
 
 function Home(){
     return(
-        <Navbar/>
+        <>
+            <Navbar/>
+            <Map/>
+        </>
+        
     );
 }
 
---
+++ app/frontend/src/pages/home/HomeComponents/Map.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect } from 'react';
+import mapboxgl from 'mapbox-gl';
+import './Map.css'; 
+
+let token = ''
+mapboxgl.accessToken = token;
+
+function Map() {
+
+    //Map Box Api call
+    useEffect(() => {
+        const map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v11', // Map style
+        center: [-74.5, 40], 
+        zoom: 9 
+        });
+        return () => map.remove();
+        }, []
+    );
+
+  return <div id="map" style={{ width: '100%', height: '100vh' }}></div>;
+}
+
+export default Map;
