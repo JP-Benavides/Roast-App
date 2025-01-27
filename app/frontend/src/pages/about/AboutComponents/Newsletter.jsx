a458191aabc7346ef6f6e8d313d08689f81c4310
SohanSondhi <sohansondhi05@gmail.com>
2025-01-27 15:26:53 -0500

    Update Newsletter.jsx

--- app/frontend/src/pages/about/AboutComponents/Newsletter.jsx
+++ app/frontend/src/pages/about/AboutComponents/Newsletter.jsx
@@ -6,11 +6,8 @@ function Newsletter(){
         <div id="NewsletterSection">
             <h3 id="NewsletterTitle">NewsLetter</h3>
             <p id="NewsletterParagraph">
-                Lorem ipsum dolor, 
-                sit amet consectetur adipisicing elit. Consequatur, 
-                qui dolorem cupiditate non harum, ad accusamus 
-                odio quidem praesentium debitis quia magni voluptates 
-                numquam quae! Officia expedita dolor quis unde?
+                Want to get coffee shop recommendations or want to hear about deals? Sign up to our weekly newsletter!
+                *Coming Soon*
             </p>
         </div>
     );
