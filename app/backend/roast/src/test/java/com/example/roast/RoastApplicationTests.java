207b2d3544065f243154907dfbe5e5ca74cb9291
JP-Benavides <149683039+JP-Benavides@users.noreply.github.com>
2025-02-05 23:11:02 -0500

    BackEnd

---
+++ app/backend/roast/src/test/java/com/example/roast/RoastApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.roast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoastApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
