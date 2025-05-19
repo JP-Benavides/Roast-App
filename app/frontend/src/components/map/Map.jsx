b7b12b4d2b34d49dd84da6058e5ed2319892435e
SohanSondhi <sohansondhi05@gmail.com>
2025-05-19 17:04:17 -0400

    added pins to test coffeeshops

--- app/frontend/src/components/map/Map.jsx
+++ app/frontend/src/components/map/Map.jsx
@@ -1,9 +1,17 @@
 import React from "react";
 import { MapContainer, TileLayer } from "react-leaflet";
+import CoffeePin from './CoffeePin';
 import "leaflet/dist/leaflet.css";
 import "./Map.css";
 
 function Map() {
+  //Test
+  const coffeeShops = [ 
+    { id: 1, name: 'Felix Roasting Co.', lat: 40.745, lon: -73.988 , rating: 4},
+    { id: 2, name: 'Maman', lat: 40.730, lon: -73.997, rating: 3},
+    { id: 3, name: 'Stumptown Coffee', lat: 40.742, lon: -73.989, rating: 2.5 }
+  ];
+
   return (
     <MapContainer 
       center={[40.7831, -73.9712]}  // Manhattan center
@@ -14,6 +22,10 @@ function Map() {
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       />
+
+      {coffeeShops.map(shop => (
+        <CoffeePin key={shop.id} shop={shop} />
+      ))}
     </MapContainer>
   );
 }
